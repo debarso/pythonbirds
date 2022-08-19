147cf7273f4780fc6a90c5124ffd15f8ecdf3f9d
debarso <debarso@live.com>
2022-08-19 12:08:54 -0300

    criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2  #atributo default/atributo de classe
+
     def __init__(self, *filhos, nome=None, idade=49):
         self.idade = idade
         self.nome = nome
@@ -19,7 +21,14 @@ if __name__ == '__main__':
         print(filho.nome)
     renzo.sobrenome = 'Nuccitelli'
     del renzo.filhos
+    renzo.olhos = 1
+    del renzo.olhos
     print(renzo.__dict__)
     print(debarso.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(debarso.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(debarso.olhos), id(renzo.olhos))
 
 
